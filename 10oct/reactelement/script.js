9885bcd3c705913c1731329dfc986fcc6ac3a5d7
Nilesh_Shirude <shirudenileshs@gmail.com>
2025-10-10 18:29:19 +0530

    study babel use

---
+++ 10oct/reactelement/script.js
@@ -0,0 +1,54 @@
+// const container=React.createElement('div',{className:'divs',id:'div1'},[
+//     React.createElement('section',{key:1,className:'sections',id:'section1'},[
+//          React.createElement('h2',{key:1,className:'subheadings',id:'subheading1'},'Itview Solution'),
+//          React.createElement('p',{key:2,className:'paragraphs',id:'para1'},'Welcome to ITview Solution, we provide the software solution to industry'),
+//          React.createElement('img',{key:3,className:'images',id:'img1',style:{width:200,background:'red',borderColor:'red'},src:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s'})
+//     ]),
+//     React.createElement('section',{key:2,className:'sections',id:'section2'},[
+//         React.createElement('h2',{key:1,className:'subheadings',id:'subheading2'},'Login Details'),
+//         React.createElement('label',{key:2,className:'labels',id:'l1',htmlFor:'username'},'UserName'),
+//         React.createElement('input',{key:3,className:'textfield',id:'textfield1'}),
+//         React.createElement('label',{key:4,className:'labels',id:'l2',htmlFor:'password'},'PassWord'),
+//         React.createElement('input',{key:5,className:'textfield',id:'textfield2',type:'password'}),
+//         React.createElement('button',{key:6,className:'buttons',id:'b1',type:'submit'},'Submit')
+
+//     ])
+// ])
+
+const container=(
+    <div className="divs" id="div1">
+      <section className="sections" id="section1">
+        <h2 className="subheadings" id="subheading1">Itview Solution</h2>
+        <p className="paragraphs" id="para1">
+          Welcome to ITview Solution, we provide the software solution to
+          industry
+        </p>
+        <img
+          className="images"
+          id="img1"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcR5U16C8yXgBpl7-Bc7Itjx3_LRl425zINA&s"
+          style={{width: 200, background:'red', borderColor:'red'}}></img>
+        
+      </section>
+      <section className="sections" id="section2">
+        <h2 className="subheadings" id="subheading2">Login Details</h2>
+        <label className="labels" id="l1" htmlFor="username" autoComplete='on'>UserName</label
+        ><input className="textfield" id="textfield1" /><label
+          className="labels"
+          id="l2"
+          htmlFor="password"
+          >PassWord</label
+        ><input className="textfield" id="textfield2" type="password" autoComplete="off" /><button
+          className="buttons"
+          id="b1"
+          type="submit"
+        >
+          Submit
+        </button>
+      </section>
+    </div>
+  )
+console.log(container)
+const ROOT=ReactDOM.createRoot(document.querySelector('#root'))
+
+ROOT.render(container)
